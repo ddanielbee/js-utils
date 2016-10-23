4ad386a765d4caf4d423470ff2716f1012f8c8f0
Daniel Bolívar <daniel@eledelab.co>
2016-10-23 20:15:58 +0200

    +MapToArray

--- js-utils/arrays_objects.js
+++ js-utils/arrays_objects.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Checks if 2 arrays contain common elements. Useful for checking adding an element only if non of the elements on a list
+ * are already present
+ * @author Daniel Bolívar <daniel.bolivar@icloud.com>
+ * @param  {Array} srcArray  Array to be tested. 
+ * @param  {Array} testArray Array to be tested against. 
+ * @return {boolean}           Wethere there were any matches.
+ */
 export function arrayContains(srcArray, testArray) {
 	let result = false;
 	testArray.forEach(value => {
@@ -8,4 +16,14 @@ export function arrayContains(srcArray, testArray) {
 		}
 	});
 	return result;
+}
+
+/**
+ * Simple wrapper across the oneliner that maps an object's property values to an array. 
+ * @author Daniel Bolívar <daniel.bolivar@icloud.com>
+ * @param  {Object} object Object to be mapped
+ * @return {Array}        Array consisting of the object's property values.
+ */
+export function mapToArray(object) {
+	return Object.keys(object).map(key => object[key]);
 }
--- tests/arrays_objects.test.js
+++ tests/arrays_objects.test.js
@@ -1,10 +1,16 @@
 'use strict';
 import * as utils from '../js-utils/arrays_objects';
 
-it('should return true since array1 and array2 both contain cats, and return false after since array2 and array3 dont have an element in common', () => {
+test('checks if 2 arrays contain common elements', () => {
 	let array1 = ['cats', 'dogs'],
 			array2 = ['cows', 'cats'],
 			array3 = ['aligators', 'polar bears'];
 	expect(utils.arrayContains(array1, array2)).toBe(true);
 	expect(utils.arrayContains(array2, array3)).toBe(false);
+});
+
+it('returns an array of the property values in an object', () => {
+	let object = {name: 'Fluffykins', species: 'cat'},
+			endArray = ['Fluffykins', 'cat'];
+	expect(utils.mapToArray(object)).toEqual(endArray);
 });
